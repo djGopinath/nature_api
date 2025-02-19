67fd6dd8e71ccd158d4531d33b142aec9863c241
djGopinath <gopinath.dj619@gmail.com>
2025-02-19 16:19:04 +0530

    create simple schema and model and did save functionality.

--- server.js
+++ server.js
@@ -18,6 +18,35 @@ mongoose
   .then(() => {
     console.log('Connected to MongoDB');
   });
+
+//  Schema Genertion 
+const tourSchema=new mongoose.Schema({
+  name:{
+    type: 'string',
+    required:[true,'A tour must have a name'],
+    unique:true
+  },
+  rating:{
+    type: Number,
+    min:1,
+    max:5,
+    default: 4.5
+  },
+  price:{
+    type: Number,
+    required:[true,'A tour must have a price'],
+  }
+});
+
+//Model generation
+const Tour=mongoose.model('Tour',tourSchema);
+
+//Create and Save a new Tour
+const newTour=new Tour({
+  name: 'The Park camper',
+  price: 499,
+});
+newTour.save().then(doc => console.log(doc)).catch(err => console.error("Couldn't save 👌"));
 const port = process.env.port || 3000;
 
 app.listen(port, () => {
