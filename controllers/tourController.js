5c03d1113c0429f873f85d0ab1cb6c385fb2b163
djGopinath <gopinath.dj619@gmail.com>
2025-02-24 11:00:57 +0530

    CRUD usign mongoose driver.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,40 +1,58 @@
-const Tour= require('../models/tourModel')
+const Tour = require('../models/tourModel');
 
-
-exports.checkBody=(req,res,next)=>{
-    if(!req.body.name || !req.body.price){
-        return res.status(400).json({ status: 'fail', message: 'Missing Name and Price' });
-    }
-    next();
-}
-
-exports.getAllTours = (req, res) => {
-  // res
-  //   .status(200)
-  //   .json({
-  //     status: 'success',
-  //     requestedAt: req.requestTime,
-  //     total_records: tours.length,
-  //     data: { tours },
-  //   });
+exports.getAllTours = async (req, res) => {
+  try {
+    // find() is used to et all record
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      total_records: tours.length,
+      data: { tours }
+    });
+  } catch (err) {
+    res.status(404).json({ status: 'Fail', message: err });
+  }
 };
-exports.getTourById = (req, res) => {
-  //   const tour = tours.find((res) => res.id === req.params.id * 1);
-  // res.status(200).json({ status: 'success', data: { tour } });
+exports.getTourById = async (req, res) => {
+  try {
+    // findById() is used to get a single record
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({ status: 'success', data: { tour } });
+  } catch (error) {
+    res.status(404).json({ status: 'fail', message: 'Tour not found' });
+  }
 };
-exports.createTour = (req, res) => {
-
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    };
-
-exports.updateTour = (req, res) => {
-  res.status(200).json({ status: 'success', data: `<Updated tour here...>` });
+exports.createTour = async (req, res) => {
+  try {
+    //create Methos is save the request body data
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
+  } catch (err) {
+    res.status(400).json({ status: 'error', message: 'Invalid Payload' });
+  }
 };
-exports.deleteTour = (req, res) => {
-  res.status(204).json({ status: 'success', data: null });
+
+exports.updateTour = async (req, res) => {
+  try {
+    // findbyIdAndUpdate is use to update the tour params : id, paylod and validation
+    const updatedTour= await Tour.findByIdAndUpdate(req.params.id, req.body,{new:true,runValidators:true})
+    res.status(200).json({ status: 'success', data: updatedTour });
+  } catch (error) {
+    res.status(400).json({ status: 'error', message: 'Invalid Payload' });
+  }
+};
+exports.deleteTour = async (req, res) => {
+  try {
+    // findbyIdAndDelete is use to delete the tour by id
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({ status: 'success', data: null });
+  } catch (error) {
+    res.status(404).json({ status: 'fail', message: 'Tour not found' });
+  }
+
 };
