8d33f3117280f327cd6a69c338f967aff390ac30
djGopinath <gopinath.dj619@gmail.com>
2025-03-06 13:11:48 +0530

    Added matching and groupig aggrigation pipelines.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -20,7 +20,6 @@ exports.getAllTours = async (req, res) => {
 
     
     const tours = await features.query;
-    console.log("API Features",tours);
     // SEND RESPONSE
     res.status(200).json({
       status: 'success',
@@ -31,7 +30,7 @@ exports.getAllTours = async (req, res) => {
     });
   } catch (err) {
     res.status(404).json({
-      status: 'failsss',
+      status: 'fail',
       message: err
     });
   }
@@ -80,3 +79,37 @@ exports.deleteTour = async (req, res) => {
   }
 
 };
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } }
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' }
+        }
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
+
--- routes/tourRoute.js
+++ routes/tourRoute.js
@@ -9,6 +9,9 @@ router
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
 router
+.route('/tour-stats').get(tourController.getTourStats) 
+
+router
   .route('/')
   .get(tourController.getAllTours)
   .post(tourController.createTour);
