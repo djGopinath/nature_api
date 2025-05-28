a56c7b991ce2c9591e1002e76be2d7ed1e718b1f
djGopinath <gopinath.dj619@gmail.com>
2025-05-28 16:39:26 +0530

    Added Global error handlers and Unhandled error

--- app.js
+++ app.js
@@ -12,12 +12,6 @@ if(process.env.NODE_ENV==='development'){
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-//middleware 
-
-app.use((req, res, next) => {
-  console.log('Middleware: Request received');
-  next();
-});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -27,5 +21,28 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours',tourRouter);
 app.use('/api/v1/users',userRouter);
+
+//To handled unhandled routes
+app.all('*',(req,res,next)=>{
+  // res.status(404).json({
+  //   status:'fail',
+  //   message:`Can't find ${req.originalUrl} on this Server!`
+  // })
+
+  const err=new Error(`Can't find ${req.originalUrl} on this Server!`);
+  err.status='fail';
+  err.statusCode=404;
+  next(err);
+})
+
+app.use((err,req,res,next)=>{
+  err.statusCode=err.statusCode || 500;
+  err.status = err.status || 'Error'
+
+  res.status(err.statusCode).json({
+    status:err.status,
+    message:err.message
+  })
+})
 
 module.exports = app;
