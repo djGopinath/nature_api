41e940aa798ac4099677bfa54b064351282f8b9b
djGopinath <gopinath.dj619@gmail.com>
2025-06-22 16:41:09 +0530

    created review model page.

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator=require('validator');
-
+// const User=require('./userModel');
 //  Schema Genertion
 const tourSchema = new mongoose.Schema({
   name: {
@@ -76,7 +76,37 @@ const tourSchema = new mongoose.Schema({
   secretTour:{
     type: Boolean,
     default: false,
-  }
+  },
+  startLocation:{
+    type:{
+      type:String,
+      default:'Point',
+      enum:['Point']
+    },
+    coordinates:[Number], 
+    address:String,
+    description:String
+  },
+   locations:[
+    {
+    type:{
+      type:String,
+      default:'Point',
+      enum:['Point']
+    },
+    coordinates:[Number], 
+    address:String,
+    description:String,
+    day:Number
+    }
+
+   ],
+   guides:[
+    {
+      type:mongoose.Schema.ObjectId,
+      ref:'User'
+    }
+   ]
 },
 {
   toJSON: { virtuals: true },
@@ -93,13 +123,26 @@ tourSchema.pre('save', function(next) {
   next();
 });
 
+// tourSchema.pre('save',async function(next) {
+// const guidesPromises=this.guides.map(async id=>await User.findById(id))
+//  this.guides= await Promise.all(guidesPromises)
+// next();
+// });
+
 tourSchema.pre(/^find/, function(next) {
   this.find({ secretTour: { $ne: true } });
 
-  // this.start = Date.now();
+   this.start = Date.now();
   next();
 });
 
+tourSchema.pre(/^find/, function(next) {
+  this.populate({
+    path:'guides',
+    select:'-__v -passwordChangedAt'
+  });
+  next();
+});
 
 tourSchema.post(/^find/, function(docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
